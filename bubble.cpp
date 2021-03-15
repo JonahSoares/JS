a728bb0769434f0a3919abfea26f6fd85ebde685
jsoares <jonahsoares2000@gmail.com>
2021-03-15 11:51:01 +0530

    bubble1

---
+++ bubble.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j,a[10],k,temp,n1,n2;
+	printf("\n\n Enter no.of elements in the array");
+	scanf("%d",&a);
+	printf("\n\nEnter the element of the array :");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+		
+	}
+	printf("\n\nThe unsorted array are \n");
